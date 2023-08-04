15e6fda3f0a675e865f64d5fb3f1849f7b4ea454
Ruslan <136455681+ruslanamr@users.noreply.github.com>
2023-08-04 14:53:15 +0600

    Добавлен выбор языка интерфейса

---
+++ src/main/java/kz/ST6/config/WebMvcConfig.java
@@ -0,0 +1,42 @@
+package kz.ST6.config;
+
+import java.util.Locale;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+  @Bean
+  public ReloadableResourceBundleMessageSource messageSource(){
+    ReloadableResourceBundleMessageSource source = new ReloadableResourceBundleMessageSource();
+    source.setBasename("classpath:messages");
+    source.setDefaultEncoding("UTF-8");
+    return source;
+  }
+
+  @Bean
+  public CookieLocaleResolver localeResolver(){
+    CookieLocaleResolver resolver = new CookieLocaleResolver();
+    resolver.setDefaultLocale(new Locale("ru"));
+    resolver.setCookieName("language");
+    resolver.setCookieMaxAge(3600*24*365);
+    return resolver;
+  }
+  @Bean
+  public LocaleChangeInterceptor localeChangeInterceptor(){
+    LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+    interceptor.setParamName("lng");
+    return interceptor;
+  }
+  @Override
+  public void addInterceptors(InterceptorRegistry registry){
+    registry.addInterceptor(localeChangeInterceptor());
+  }
+
+}
--- src/main/java/kz/ST6/services/RequestService.java
+++ src/main/java/kz/ST6/services/RequestService.java
@@ -2,14 +2,13 @@ package kz.ST6.services;
 
 import java.util.List;
 import kz.ST6.models.AppRequest;
-import org.springframework.stereotype.Service;
 
-@Service
+
+
 public interface RequestService {
   AppRequest addRequest(AppRequest request);
   List<AppRequest> getListRequests();
   void deleteRequest(Long id);
   AppRequest getReqById(Long id);
   AppRequest updateRequest(AppRequest request);
-
 }
